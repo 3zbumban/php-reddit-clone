14a83cab308845d49198b57a4816948336fe5584
3zbumban <free4allcoding@gmail.com>
2022-04-27 19:47:05 +0200

    client user service

--- client/src/service/user.service.js
+++ client/src/service/user.service.js
@@ -1,33 +1,43 @@
 import apiUrl from "./apiUrl";
 
-const login = (payload) => {
-  try {
-    const response = await fetch(`${apiBase}/auth/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "access-control-request-headers": "Content-Type"
-      },
-      body: JSON.stringify(payload)
-    })
-    return await response.json()
-  } catch (error) {
-    throw error
-  }
+const signup = async (payload) => {
+  const response = await fetch(`${apiUrl}/auth/signup`, {
+    method: "POST",
+    headers: {
+      // "Content-Type": "application/json",
+      // "access-control-request-headers": "Content-Type"
+    },
+    body: JSON.stringify(payload)
+  })
+  return await response.json()
 }
 
-const signup = () => {
-  try {
-    
-  } catch (error) {
-    
-  }
+const login = async (payload) => {
+  const response = await fetch(`${apiUrl}/auth/login`, {
+    method: "POST",
+    headers: {
+      // "Content-Type": "application/json",
+      // "access-control-request-headers": "Content-Type"
+    },
+    body: JSON.stringify(payload)
+  })
+  return await response.json()
 }
 
-const refreshToken = () => {
-  try {
-    
-  } catch (error) {
-    
-  }
+const refreshToken = async (payload, userId) => {
+  const response = await fetch(`${apiUrl}/auth/refresh?userId=${userId}`, {
+    method: "POST",
+    headers: {
+      // "Content-Type": "application/json",
+      // "access-control-request-headers": "Content-Type"
+    },
+    body: JSON.stringify(payload)
+  })
+  return await response.json()
+}
+
+export default {
+  signup,
+  login,
+  refreshToken
 }

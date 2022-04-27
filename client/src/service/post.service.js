e94bb0d08bcfcb98f181717b0d01bf94c6143859
3zbumban <free4allcoding@gmail.com>
2022-04-27 22:12:08 +0200

    🧹

--- client/src/service/post.service.js
+++ client/src/service/post.service.js
@@ -1,37 +1,29 @@
 import apiUrl from "./apiUrl";
 
 const getAll = async (threadId) => {
-  try {
-    const response = await fetch(`${apiUrl}/post?threadId=${threadId}`, {
-      method: "GET",
-      headers: {
-        // "Content-Type": "application/json",
-        // "Accept": "application/json",
-        // "access-control-request-headers": "Content-Type"
-      }
-    });
-    return await response.json();
-  } catch (error) {
-    
-  }
+  const response = await fetch(`${apiUrl}/post?threadId=${threadId}`, {
+    method: "GET",
+    headers: {
+      // "Content-Type": "application/json",
+      // "Accept": "application/json",
+      // "access-control-request-headers": "Content-Type"
+    }
+  });
+  return await response.json();
 }
 
 // http://localhost:3030/post/dfd72475-fa80-4377-8981-0a54bb320853
 
 const getOne = async (postId) => {
-  try {
-    const response = await fetch(`${apiUrl}/post/${postId}`, {
-      method: "GET",
-      headers: {
-        // "Content-Type": "application/json",
-        // "Accept": "application/json",
-        // "access-control-request-headers": "Content-Type"
-      }
-    });
-    return await response.json();
-  } catch (error) {
-    throw error;
-  }
+  const response = await fetch(`${apiUrl}/post/${postId}`, {
+    method: "GET",
+    headers: {
+      // "Content-Type": "application/json",
+      // "Accept": "application/json",
+      // "access-control-request-headers": "Content-Type"
+    }
+  });
+  return await response.json();
 }
 
 const vote = async (postId, userId, vote) => {

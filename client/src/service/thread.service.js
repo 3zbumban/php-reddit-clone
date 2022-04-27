5b3adccf5e67a04c9f98595a364941e06ce73105
3zbumban <free4allcoding@gmail.com>
2022-04-27 19:46:57 +0200

    client thread service

--- client/src/service/thread.service.js
+++ client/src/service/thread.service.js
@@ -1,29 +1,30 @@
 import apiUrl from "./apiUrl";
 
-const getAll = async () => {
-  try {
-    const response = await fetch(`${apiUrl}/thread`, {
-      method: "GET",
-      headers: {
-        // "Content-Type": "application/json", // todo: wtf?
-        // "Accept": "application/json",
-        // "access-control-request-headers": "Content-Type"
-      }
-    });
-    return await response.json();
-  } catch (error) {
-    throw error;
-  }
+const create = async (payload) => {
+  const response = await fetch(`${apiUrl}/thread`, {
+    method: "POST",
+    headers: {
+      // "Content-Type": "application/json",
+      // "access-control-request-headers": "Content-Type"
+    },
+    body: JSON.stringify(payload)
+  })
+  return await response.json()
 }
 
-const create = () => {
-  try {
-    
-  } catch (error) {
-    
-  }
+const getAll = async () => {
+  const response = await fetch(`${apiUrl}/thread`, {
+    method: "GET",
+    headers: {
+      // "Content-Type": "application/json", // todo: wtf?
+      // "Accept": "application/json",
+      // "access-control-request-headers": "Content-Type"
+    }
+  });
+  return await response.json();
 }
 
 export default {
   getAll,
+  create
 }

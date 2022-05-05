ac97e7093f911cb4941caa51730759ea1f0b0b7e
3zbumban <free4allcoding@gmail.com>
2022-05-05 21:35:23 +0200

    auth store

--- client/src/store.js
+++ client/src/store.js
@@ -9,18 +9,26 @@ export const useStore = defineStore('main', {
     },
   }),
   actions: {
-    authenticate(id, name) {
-      this.user.id = id
-      this.user.name = name
+    async authenticate(user) {
+      this.user.id = user.userUid
+      this.user.name = user.username
       this.user.loggedIn = true
+      const jwt = user.jwt
+      console.log(jwt)
+      localStorage.setItem('user', JSON.stringify(this.user))
+      localStorage.setItem('jwt', jwt)
     },
     unAuthenticate() {
       this.user.id = ''
       this.user.name = ''
       this.user.loggedIn = false
+      localStorage.removeItem('user')
+      localStorage.removeItem('jwt')
     },
     async reAuthenticate() {
-
+      const user = JSON.parse(localStorage.getItem('user'))
+      const jwt = localStorage.getItem('jwt')
+      
     }
   },
 });

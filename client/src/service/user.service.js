252c4c460a6e8e8ae3ef37b146b4e7fbc28cfbc3
3zbumban <free4allcoding@gmail.com>
2022-05-06 21:35:01 +0200

    tryand catch

--- client/src/service/user.service.js
+++ client/src/service/user.service.js
@@ -2,32 +2,44 @@ import apiUrl from "./apiUrl.js";
 import getToken from "./getToken.js";
 
 const signup = async (payload) => {
-  const response = await fetch(`${apiUrl}/auth/signup`, {
-    method: "POST",
-    headers: {},
-    body: JSON.stringify(payload)
-  })
-  return await response.json()
+  try {
+    const response = await fetch(`${apiUrl}/auth/signup`, {
+      method: "POST",
+      headers: {},
+      body: JSON.stringify(payload)
+    })
+    return await response.json()
+  } catch (e) {
+    throw e
+  }
 }
 
 const login = async (payload) => {
-  const response = await fetch(`${apiUrl}/auth/login`, {
-    method: "POST",
-    headers: {},
-    body: JSON.stringify(payload)
-  })
-  return await response.json()
+  try {
+    const response = await fetch(`${apiUrl}/auth/login`, {
+      method: "POST",
+      headers: {},
+      body: JSON.stringify(payload)
+    })
+    return await response.json()
+  } catch (e) {
+    throw e
+  }
 }
 
 const refreshToken = async (userId) => {
-  const response = await fetch(`${apiUrl}/auth/refresh?userId=${userId}`, {
-    method: "POST",
-    headers: {
-      "Access-Token": getToken(),
-      "Access-Control-Request-Headers": "Access-Token"
-    }
-  })
-  return await response.json()
+  try {
+    const response = await fetch(`${apiUrl}/auth/refresh?userId=${userId}`, {
+      method: "POST",
+      headers: {
+        "Access-Token": getToken(),
+        "Access-Control-Request-Headers": "Access-Token"
+      }
+    })
+    return await response.json()
+  } catch (e) {
+    throw e
+  }
 }
 
 export default {

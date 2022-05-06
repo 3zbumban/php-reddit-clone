3995662de6463ca27a34c51ac8a8a77fd6b6b176
3zbumban <free4allcoding@gmail.com>
2022-05-06 21:35:17 +0200

    try and catch

--- client/src/service/comment.service.js
+++ client/src/service/comment.service.js
@@ -2,15 +2,19 @@ import apiUrl from "./apiUrl.js";
 import getToken from "./getToken.js";
 
 const comment = async (payload, userId, postId) => {
-  const response = await fetch(`${apiUrl}/comment?postId=${postId}&userId=${userId}`, {
-    method: "POST",
-    headers: {
-      "Access-Token": getToken(),
-      "Access-Control-Request-Headers": "Access-Token"
-    },
-    body: JSON.stringify(payload)
-  });
-  return await response.json();
+  try {
+    const response = await fetch(`${apiUrl}/comment?postId=${postId}&userId=${userId}`, {
+      method: "POST",
+      headers: {
+        "Access-Token": getToken(),
+        "Access-Control-Request-Headers": "Access-Token"
+      },
+      body: JSON.stringify(payload)
+    });
+    return await response.json()
+  } catch (e) {
+    throw e
+  }
 }
 
 export default {

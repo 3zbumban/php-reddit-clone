ef3f6148ec4fdc14b90a72d37ea4068bb5e71ee0
3zbumban <free4allcoding@gmail.com>
2022-05-06 20:32:50 +0200

    wip comment auth

--- app/src/Controller/CommentController.php
+++ app/src/Controller/CommentController.php
@@ -6,6 +6,7 @@ use Exception;
 use Sem\Weben\Http\RequestInterface;
 use Sem\Weben\Http\ResponseInterface;
 use Sem\Weben\Service\CommentService;
+use Sem\Weben\Service\UserService;
 
 class CommentController
 {
@@ -15,14 +16,21 @@ class CommentController
     // todo: auth - user uuid
     $query = $req->getQueryParams();
     $body = $req->getBody();
+    $header = $req->getHeader();
+
     if (empty($query['postId']) || empty($query['userId']) || empty($body['text'])) {
       throw new Exception('Missing parameters');
     }
+
+    if (empty($header['access-token'])) {
+      throw new Exception('Missing authorization header');
+    }
     $postId = $query["postId"];
     $userId = $query["userId"];
-    $text = $req->getBody()["text"];
+    $text = $body["text"];
+    $jwt = $header["access-token"];
     
-    // $authenticated = UserService::checkJwtAndUser();
+    $user = UserService::checkJwtAndUser($jwt, $userId);
     $comment = CommentService::commentOnPost($postId, $text, $userId);
 
     $res->setStatusCode(200);

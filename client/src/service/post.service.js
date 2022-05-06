ffb6275d4ede1bf73908a0c86fbee4fbd7362176
3zbumban <free4allcoding@gmail.com>
2022-05-06 21:35:13 +0200

    try and catch

--- client/src/service/post.service.js
+++ client/src/service/post.service.js
@@ -2,42 +2,58 @@ import apiUrl from "./apiUrl.js";
 import getToken from "./getToken.js";
 
 const getAll = async (threadId) => {
-  const response = await fetch(`${apiUrl}/post?threadId=${threadId}`, {
-    method: "GET",
-    headers: {}
-  });
-  return await response.json();
+  try {
+    const response = await fetch(`${apiUrl}/post?threadId=${threadId}`, {
+      method: "GET",
+      headers: {}
+    });
+    return await response.json();
+  } catch (e) {
+    throw e
+  }
 }
 
 const getOne = async (postId) => {
-  const response = await fetch(`${apiUrl}/post/${postId}`, {
-    method: "GET",
-    headers: {}
-  });
-  return await response.json();
+  try {
+    const response = await fetch(`${apiUrl}/post/${postId}`, {
+      method: "GET",
+      headers: {}
+    });
+    return await response.json();
+  } catch (e) {
+    throw e
+  }
 }
 
 const vote = async (postId, userId, vote) => {
-  const response = await fetch(`${apiUrl}/vote?postId=${postId}&userId=${userId}&vote=${vote}`, {
-    method: "POST",
-    headers: {
-      "Access-Token": getToken(),
-      "Access-Control-Request-Headers": "Access-Token"
-    }
-  });
-  return await response.json();
+  try {
+    const response = await fetch(`${apiUrl}/vote?postId=${postId}&userId=${userId}&vote=${vote}`, {
+      method: "POST",
+      headers: {
+        "Access-Token": getToken(),
+        "Access-Control-Request-Headers": "Access-Token"
+      }
+    });
+    return await response.json();
+  } catch (e) {
+    throw e
+  }
 }
 
 const create = async (payload) => {
-  const response = await fetch(`${apiUrl}/post`, {
-    method: "POST",
-    headers: {
-      "Access-Token": getToken(),
-      "Access-Control-Request-Headers": "Access-Token"
-    },
-    body: JSON.stringify(payload)
-  });
-  return await response.json();
+  try {
+    const response = await fetch(`${apiUrl}/post`, {
+      method: "POST",
+      headers: {
+        "Access-Token": getToken(),
+        "Access-Control-Request-Headers": "Access-Token"
+      },
+      body: JSON.stringify(payload)
+    });
+    return await response.json();
+  } catch (e) {
+    throw e
+  }
 }
 
 export default {
